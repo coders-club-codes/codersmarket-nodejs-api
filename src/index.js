9916e57ae4371df292e88d570b2ca0d5bb8db612
WellsSA <wel.cavzod@gmail.com>
2021-10-22 14:03:15 -0300

    feat: using cors and express.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import 'dotenv/config';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({ origin: '*' }));
+app.use(express.json());
 
 const port = process.env.NODE_PORT;
 
