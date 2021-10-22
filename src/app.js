fc71425e97c2e3c4ebfe77309d47a614ccfd6c8e
WellsSA <wel.cavzod@gmail.com>
2021-10-22 14:18:22 -0300

    feat: create product route definition using proper HTTP status

--- src/app.js
+++ src/app.js
@@ -10,17 +10,17 @@ app.use(express.json());
 // CUIDADO: ☢️ DAQUI PRA BAIXO É TUDO MOCK ☢️
 const MOCK_PRODUCTS = [
   {
-    id: '1',
+    id: '0',
     name: 'Arroz 1k marca X',
     price: 5.7,
   },
   {
-    id: '2',
+    id: '1',
     name: 'Feijão 1k marca Y',
     price: 10.7,
   },
   {
-    id: '3',
+    id: '2',
     name: 'Danoninho 12 unidades',
     price: 15,
   },
@@ -34,19 +34,47 @@ function findProductById(id) {
   const product = MOCK_PRODUCTS.find(product => product.id === id);
   return product;
 }
+
+function createProduct(product) {
+  const newProduct = {
+    id: MOCK_PRODUCTS.length.toString(),
+    ...product,
+  };
+  MOCK_PRODUCTS.push(newProduct);
+
+  return newProduct;
+}
 // CUIDADO: ☢️ DAQUI PRA CIMA É TUDO MOCK ☢️
 
 app.get('/products', (req, res) => {
   const products = listAllProducts();
-  return res.json(products);
+
+  // Status 200: OK Success
+  return res.status(200).json(products);
 });
 
+// /routeEntity/:routeParam
 app.get('/products/:id', (req, res) => {
   const product = findProductById(req.params.id);
-  return res.json(product);
+  return res.status(200).json(product);
 });
 
-app.post('/products', (req, res) => res.json({ success: true }));
+app.post('/products', (req, res) => {
+  const { name, price } = req.body;
+
+  if (price < 0) {
+    // Status 400: Bad request
+    return res.status(400).json({
+      success: false,
+      error: 'Product price cannot be a negative value',
+    });
+  }
+  const newProduct = createProduct({ name, price });
+
+  // Status 201: Created
+  return res.status(201).json(newProduct);
+});
+
 app.put('/products', (req, res) => res.json({ success: true }));
 app.delete('/products', (req, res) => res.json({ success: true }));
 
