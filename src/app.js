f11c8d6e1d3d5841278e3446b5a34eb46f8be174
WellsSA <wel.cavzod@gmail.com>
2021-10-22 14:05:57 -0300

    feat: first routes definition

--- src/app.js
+++ src/app.js
@@ -7,4 +7,9 @@ const app = express();
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 
+app.get('/products', (req, res) => res.json({ success: true }));
+app.post('/products', (req, res) => res.json({ success: true }));
+app.put('/products', (req, res) => res.json({ success: true }));
+app.delete('/products', (req, res) => res.json({ success: true }));
+
 export default app;
