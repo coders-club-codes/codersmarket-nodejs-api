9b8ed3cfa0db3485564d872b4567506c1863cabd
WellsSA <wel.cavzod@gmail.com>
2021-10-22 14:08:55 -0300

    feat: /products GET definition with product data mocks

--- src/app.js
+++ src/app.js
@@ -7,7 +7,35 @@ const app = express();
 app.use(cors({ origin: '*' }));
 app.use(express.json());
 
-app.get('/products', (req, res) => res.json({ success: true }));
+// CUIDADO: ☢️ DAQUI PRA BAIXO É TUDO MOCK ☢️
+const MOCK_PRODUCTS = [
+  {
+    id: '1',
+    name: 'Arroz 1k marca X',
+    price: 5.7,
+  },
+  {
+    id: '2',
+    name: 'Feijão 1k marca Y',
+    price: 10.7,
+  },
+  {
+    id: '3',
+    name: 'Danoninho 12 unidades',
+    price: 15,
+  },
+];
+
+function listAllProducts() {
+  return MOCK_PRODUCTS;
+}
+// CUIDADO: ☢️ DAQUI PRA CIMA É TUDO MOCK ☢️
+
+app.get('/products', (req, res) => {
+  const products = listAllProducts();
+  return res.json(products);
+});
+
 app.post('/products', (req, res) => res.json({ success: true }));
 app.put('/products', (req, res) => res.json({ success: true }));
 app.delete('/products', (req, res) => res.json({ success: true }));
