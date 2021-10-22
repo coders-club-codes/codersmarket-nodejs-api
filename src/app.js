3ae1442a092650b6f26f282e5e45f2248a63a6a0
WellsSA <wel.cavzod@gmail.com>
2021-10-22 14:11:17 -0300

    feat: product find route definition

--- src/app.js
+++ src/app.js
@@ -29,11 +29,21 @@ const MOCK_PRODUCTS = [
 function listAllProducts() {
   return MOCK_PRODUCTS;
 }
+
+function findProductById(id) {
+  const product = MOCK_PRODUCTS.find(product => product.id === id);
+  return product;
+}
 // CUIDADO: ☢️ DAQUI PRA CIMA É TUDO MOCK ☢️
 
 app.get('/products', (req, res) => {
   const products = listAllProducts();
   return res.json(products);
+});
+
+app.get('/products/:id', (req, res) => {
+  const product = findProductById(req.params.id);
+  return res.json(product);
 });
 
 app.post('/products', (req, res) => res.json({ success: true }));
